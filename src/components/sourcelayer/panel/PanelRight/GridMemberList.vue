96361fafe8399a9adb9177e8ed67e699011c12f3
EdisonForMan <chuanghewzh12@hotmail.com>
2020-11-25 17:13:26 +0800

    网格员可搜索

--- src/components/sourcelayer/panel/PanelRight/GridMemberList.vue
+++ src/components/sourcelayer/panel/PanelRight/GridMemberList.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="grid-member-list">
-    <header class="ph-right">网格员信息</header>
+    <header class="ph-right">
+      网格员信息
+      <div class="searchHeader">
+        <div class="button-item">
+          <i class="icon-common icon-search"></i>
+        </div>
+        <el-input
+          v-model="searchText"
+          class="searchFilterInput"
+          placeholder="输入网格员名称"
+          size="small"
+        />
+        <div class="button-container">
+          <div class="button-item">
+            <i class="icon-common icon-clear" @click="searchClear"></i>
+          </div>
+        </div>
+      </div>
+    </header>
     <div class="grid-member-list-ul">
       <header>
         <span>序号</span>
@@ -10,7 +28,7 @@
       </header>
       <ul>
         <li
-          v-for="(item, i) in gridMemberList"
+          v-for="(item, i) in fixGridMemberList"
           :key="i"
           @click="doForceGridMember(item)"
         >
@@ -30,13 +48,21 @@ import CIM_API from "api/cimAPI";
 export default {
   name: "gridMemberList",
   data() {
-    return {};
+    return { searchText: "", fixGridMemberList: [] };
   },
   computed: {
     ...mapGetters("map", ["gridMemberList"]),
   },
-  mounted() {
-    this.setGridMemberList();
+  watch: {
+    searchText(n) {
+      this.fixGridMemberList = this.gridMemberList.filter(
+        (v) => ~v.NAME.indexOf(n)
+      );
+    },
+  },
+  async created() {
+    await this.setGridMemberList();
+    this.fixGridMemberList = this.gridMemberList;
   },
   methods: {
     ...mapActions("map", ["setForceGridMember", "setGridMemberList"]),
@@ -49,13 +75,63 @@ export default {
             message: "无网格员巡逻信息",
           });
     },
+    searchClear() {
+      this.searchText = "";
+    },
   },
 };
 </script>
 
-<style lang="less" scoped>
+<style lang="less">
 .grid-member-list {
   flex: 1;
+  .ph-right {
+    .searchHeader {
+      display: inline-flex;
+      align-items: center;
+      box-sizing: border-box;
+      border-radius: 2vh;
+      background-color: rgba(0, 0, 255, 0.2);
+      border: 1px solid rgba(0, 0, 255, 0.8);
+      padding: 0 0.6vh;
+      width: 28vh;
+
+      > * {
+        height: 2.6vh;
+        vertical-align: middle;
+        > * {
+          display: block;
+          height: 100%;
+        }
+      }
+
+      .searchFilterInput {
+        flex: 1;
+        .el-input__inner {
+          background: none;
+          border: none;
+          color: white;
+          padding: 0 1vh;
+          height: 2.4vh;
+        }
+      }
+
+      .icon-common {
+        display: block;
+        width: 2.4vh;
+        height: 2.4vh;
+      }
+
+      .icon-clear {
+        .bg-image("/static/images/common/clear");
+        cursor: pointer;
+      }
+
+      .icon-search {
+        .bg-image("/static/images/common/search2");
+      }
+    }
+  }
   .grid-member-list-ul {
     flex: 1;
     color: #fff;
